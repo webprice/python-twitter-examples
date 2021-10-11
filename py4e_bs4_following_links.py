b704fc34c933974b6c109efeab3ac31b2e96da3a
webprice <out455@ya.ru>
2021-10-11 19:14:13 +0300

    Create py4e_bs4_following_links.py
    Actual problem: Start at: http://py4e-data.dr-chuck.net/known_by_Sergei.html
    Find the link at position 18 (the first name is 1). Follow that link. Repeat this process 7 times. The answer is the last name that you retrieve.
    Hint: The first character of the name of the last page that you will load is: H
    Strategy
    The web pages tweak the height between the links and hide the page after a few seconds to make it difficult for you to do the assignment without writing a Python program. But frankly with a little effort and patience you can overcome these attempts to make it a little harder to complete the assignment without writing a Python program. But that is not the point. The point is to write a clever Python program to solve the program.
    
    Sample execution
    
    Here is a sample execution of a solution:
    
    $ python3 solution.py
    Enter URL: http://py4e-data.dr-chuck.net/known_by_Fikret.html
    Enter count: 4
    Enter position: 3
    Retrieving: http://py4e-data.dr-chuck.net/known_by_Fikret.html
    Retrieving: http://py4e-data.dr-chuck.net/known_by_Montgomery.html
    Retrieving: http://py4e-data.dr-chuck.net/known_by_Mhairade.html
    Retrieving: http://py4e-data.dr-chuck.net/known_by_Butchi.html
    Retrieving: http://py4e-data.dr-chuck.net/known_by_Anayah.html
    The answer to the assignment for this execution is "Anayah".

---
+++ py4e_bs4_following_links.py
@@ -0,0 +1,27 @@
+# To run this, download the BeautifulSoup zip file
+# http://www.py4e.com/code3/bs4.zip
+# and unzip it in the same directory as this file
+
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter the link- ')
+position = int(input("Enter position:"))
+repeat = int(input("How many times to repeat:"))
+
+
+while repeat != 0:
+    html = urllib.request.urlopen(url, context=ctx).read()
+    soup = BeautifulSoup(html, 'html.parser')
+
+# Retrieve all of the anchor tags
+    tags = soup('a')
+    url = tags[position].get('href',None)
+    print(url)
+    repeat = repeat-1
